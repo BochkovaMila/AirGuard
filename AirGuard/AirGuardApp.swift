e61a77276ed5c14375b9a0af31a27c9751d422a1
Mila B <milabochkova03@gmail.com>
2023-10-17 13:51:52 +0300

    Initial Commit

---
+++ AirGuard/AirGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirGuardApp.swift
+//  AirGuard
+//
+//  Created by Mila B on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AirGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
