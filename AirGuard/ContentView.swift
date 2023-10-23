0878936a96fa9a882602bd583e21bb9816f4eb56
Mila B <milabochkova03@gmail.com>
2023-10-23 09:20:51 +0300

    clean up

--- AirGuard/ContentView.swift
+++ AirGuard/ContentView.swift
@@ -11,13 +11,6 @@ import MapKit
 struct ContentView: View {
     @StateObject var viewModel: ContentViewModel
     
-    let annotations = [
-            CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-            CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508),
-            CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5),
-            CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667)
-        ]
-    
     var body: some View {
         
         Map(coordinateRegion: $viewModel.locationManager.region, showsUserLocation: true, annotationItems: viewModel.searchResults) { point in
