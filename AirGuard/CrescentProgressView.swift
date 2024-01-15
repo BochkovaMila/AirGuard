71460ef115f479ee3206321f53799ecc5e1dc063
Mila B <milabochkova03@gmail.com>
2024-01-15 13:44:19 +0300

    add progress view for aqi on CurrentDataView

---
+++ AirGuard/CrescentProgressView.swift
@@ -0,0 +1,43 @@
+//
+//  CrescentProgressView.swift
+//  AirGuard
+//
+//  Created by Mila B on 12.01.2024.
+//
+
+import SwiftUI
+
+struct CrescentProgressView: View {
+    
+    let progress: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .trim(from: 0, to: 0.5)
+                .stroke(
+                    Color.pink.opacity(0.5),
+                    style: StrokeStyle(
+                        lineWidth: 20,
+                        lineCap: .round
+                    )
+                )
+                .rotationEffect(.degrees(-180))
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(
+                    Color.pink,
+                    style: StrokeStyle(
+                        lineWidth: 20,
+                        lineCap: .round
+                    )
+                )
+                .rotationEffect(.degrees(-180))
+                .animation(.easeOut, value: progress)
+        }
+    }
+}
+
+#Preview {
+    CrescentProgressView(progress: 0.3)
+}
--- AirGuard/CurrentDataView.swift
+++ AirGuard/CurrentDataView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct CurrentDataView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Моя локация")
+                .padding(.bottom, 50)
+            ZStack {
+                CrescentProgressView(progress: 0.1)
+                    .frame(width: 200, height: 200)
+                VStack {
+                    HStack {
+                        Text("-1")
+                            .font(Font.system(size: 28))
+                            .bold()
+                        Text("AQI")
+                    }
+                    HStack {
+                        Text("😄")
+                        Text("Отлично")
+                    }
+                }
+                .padding(.top, -30)
+            }
+            
+        }
     }
 }
 
